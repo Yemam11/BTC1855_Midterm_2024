94d3baa78976c63bd66170e55b9623723e48b2a2
Yemam11 <1youssefemam@gmail.com>
2024-07-31 15:24:25 -0400

    Cleaned stations dataset

--- main.R
+++ main.R
@@ -7,3 +7,21 @@
 stations <- read.csv("station.csv")
 trips <- read.csv("trip.csv")
 weather <- read.csv("weather.csv")
+
+#import libraries
+library(lubridate)
+library(tidyverse)
+
+# Clean the stations dataset
+
+#no NAs
+apply(is.na(stations), 2, sum)
+
+# Summarize the data
+# doesnt seem to have any outliers
+summary(stations)
+
+#convert installation date to posixct format
+stations$installation_date <- mdy(stations$installation_date, tz = "UTC")
+
+
